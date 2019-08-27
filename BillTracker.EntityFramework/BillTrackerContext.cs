a09be6222a2e973647a8f502fa8842ac0b9d2c5e
Brayan mota <bamotav@gmail.com>
2019-08-27 15:57:23 -0400

    Added Dbcontext Models

--- BillTracker.EntityFramework/BillTrackerContext.cs
+++ BillTracker.EntityFramework/BillTrackerContext.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using BillTracker.Model;
+using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -15,6 +16,21 @@ namespace BillTracker.EntityFramework
         {
         }
 
+        public virtual DbSet<Income> Incomes { get; set; }
+
+        public virtual DbSet<IncomeCalendar> IncomeCalendars { get; set; }
+
+        public virtual DbSet<Expense> Expenses { get; set; }
+
+        public virtual DbSet<EspenseCalendar> EspenseCalendars { get; set; }
+
+        public virtual DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+
+        public virtual DbSet<AccountBank> AccountsBanks { get; set; }
+
+        public virtual DbSet<Bill> Bills { get; set; }
+
+        public virtual DbSet<BillItems> BillItems { get; set; }
 
     }
 }
