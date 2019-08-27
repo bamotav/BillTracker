211e76ae18afcabe4fc969ac03757443cb30409b
Brayan mota <bamotav@gmail.com>
2019-08-27 16:59:40 -0400

    Added Reference Project to Model

---
+++ BillTracker.Model/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BillTracker.Model
+{
+    public abstract class BaseModel
+    {
+        [Key]
+        public virtual int Id { get; set; }
+    }
+}
