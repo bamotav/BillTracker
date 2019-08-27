4b9ebd01125aedcef3d13e62868f4eaac20b5191
Brayan mota <bamotav@gmail.com>
2019-08-27 16:58:21 -0400

    Changes Behavior ForeignKeys Delete Cascade To Restrict

--- BillTracker.EntityFramework/BillTrackerContext.cs
+++ BillTracker.EntityFramework/BillTrackerContext.cs
@@ -1,8 +1,9 @@
 ﻿using BillTracker.Model;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
-
+using System.Linq;
 
 namespace BillTracker.EntityFramework
 {
@@ -14,6 +15,25 @@ namespace BillTracker.EntityFramework
 
         protected BillTrackerContext()
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                // equivalent of modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+                entityType.Relational().TableName = entityType.DisplayName();
+
+                // equivalent of modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+                // and modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+                entityType.GetForeignKeys()
+                    .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+                    .ToList()
+                    .ForEach(fk => fk.DeleteBehavior = DeleteBehavior.Restrict);
+            }
+
+
+            base.OnModelCreating(modelBuilder);
         }
 
         public virtual DbSet<Income> Incomes { get; set; }
