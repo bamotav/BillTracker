845185ba126866de3e7d73da82f87d27dde8aecd
Brayan mota <bamotav@gmail.com>
2019-08-27 15:31:42 -0400

    Added Project BillTracker.EntityFramework and DbContext Class

---
+++ BillTracker.EntityFramework/BillTrackerContext.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace BillTracker.EntityFramework
+{
+    public class BillTrackerContext : DbContext
+    {
+        public BillTrackerContext(DbContextOptions<BillTrackerContext> options) : base(options)
+        {
+        }
+
+        protected BillTrackerContext()
+        {
+        }
+
+
+    }
+}
