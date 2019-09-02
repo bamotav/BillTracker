ddf6edd68bd5d80b2e455b8cba12403cb1dece84
Brayan mota <bamotav@gmail.com>
2019-09-02 15:35:34 -0400

    Added project Infraestructure, Generic repository and UnitOfWork Patterns

---
+++ BillTracker.Infraestructure/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BillTracker.Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+
+        Task<int> Commit();
+
+        void Rollback();
+    }
+}
---
+++ BillTracker.Infraestructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using BillTracker.EntityFramework;
+using BillTracker.Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillTracker.Infraestructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BillTrackerContext _dbContext;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
+        public Dictionary<Type, object> Repositories
+        {
+            get { return _repositories; }
+            set { Repositories = value; }
+        }
+
+        public UnitOfWork(BillTrackerContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            if (Repositories.Keys.Contains(typeof(T)))
+            {
+                return Repositories[typeof(T)] as IGenericRepository<T>;
+            }
+
+            IGenericRepository<T> repo = new GenericRepository<T>(_dbContext);
+            Repositories.Add(typeof(T), repo);
+            return repo;
+        }
+
+        public async Task<int> Commit()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Rollback()
+        {
+            _dbContext.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+        }
+    }
+}
